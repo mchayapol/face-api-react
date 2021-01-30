2801c2d51ba8465fd7f5a6531f19de5ed68adfb9
Chayapol Moemeng <mchayapol@scitech.au.edu>
2021-01-30 18:20:14 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Face Landmarks with React + face-api.js
+This app uses webcam and (face-api.js)[https://github.com/justadudewhohacks/face-api.js/] to detect a face and extract landmarks of the face.
+
+To run
+```
+yarn
+yarn start
+```
+and browse to http://localhost:3000
---
+++ src/App.js
@@ -0,0 +1,67 @@
+import React, { useState, useEffect, useRef } from 'react'
+import * as faceapi from 'face-api.js'
+import Webcam from 'react-webcam';
+import './App.css';
+
+const MODEL_URL = '/models'
+
+const videoConstraints = {
+  width: 350,
+  height: 350,
+  facingMode: 'user',
+};
+
+export default function App() {
+  const overlayCanvas = useRef()
+  const webcam = useRef()
+
+  useEffect(() => {
+    loadModels()
+  })
+
+  let [withBoxes, setWithBoxes] = useState(true)
+
+  async function loadModels() {
+    await faceapi.loadFaceDetectionModel(MODEL_URL)
+    await faceapi.loadFaceLandmarkModel(MODEL_URL)
+    await faceapi.loadFaceRecognitionModel(MODEL_URL)
+    await faceapi.loadTinyFaceDetectorModel(MODEL_URL)
+    await faceapi.loadSsdMobilenetv1Model(MODEL_URL)
+  }
+
+  setInterval(async () => {
+    try {
+      // Webcam could drop out occasionally, better put in try-catch block
+      const video = webcam.current.video
+      const canvas = overlayCanvas.current
+
+      let options = new faceapi.TinyFaceDetectorOptions()
+
+      const result = await faceapi.detectSingleFace(video, options).withFaceLandmarks()
+      if (result) {
+        const dims = faceapi.matchDimensions(canvas, video, true)
+        const resizedResult = faceapi.resizeResults(result, dims)
+
+        if (withBoxes) {
+          faceapi.draw.drawDetections(canvas, resizedResult)
+        }
+        faceapi.draw.drawFaceLandmarks(canvas, resizedResult)
+      }
+    } catch (err) {
+      console.err(err)
+    }
+  }, 100)
+
+  return (
+    <div>
+      <Webcam
+        audio={false}
+        ref={webcam}
+        screenshotFormat="image/jpeg"
+        videoConstraints={videoConstraints}
+        className="webcam"
+      />
+      <canvas className="overlay" ref={overlayCanvas} />
+    </div>
+  )
+}
